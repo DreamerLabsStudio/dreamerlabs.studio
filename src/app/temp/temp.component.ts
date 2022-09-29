2fdef1df97c4c6d8e95dd6e5fea61baea6b3d499
Naomi Carrigan <nhcarrigan@gmail.com>
2022-09-28 23:25:38 -0700

    feat: workflows

--- src/app/temp/temp.component.ts
+++ src/app/temp/temp.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-temp',
   templateUrl: './temp.component.html',
-  styleUrls: ['./temp.component.css']
+  styleUrls: ['./temp.component.css'],
 })
-export class TempComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TempComponent {
+  constructor() {}
 }
